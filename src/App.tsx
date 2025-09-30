10a80d75cadd9915805a3930e0d3c23408ea780a
DDefty <dawid.kogut99@gmail.com>
2025-09-30 17:28:13 +0200

    Implement movie search functionality in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 export const App: React.FC = () => {
+  const [filter, setFilter] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilter(event.target.value);
+  };
+
+  const visibleMovies = [...moviesFromServer].filter(
+    movie =>
+      movie.title.toLowerCase().includes(filter.trim().toLowerCase()) ||
+      movie.description.toLowerCase().includes(filter.trim().toLowerCase()),
+  );
+
   return (
     <div className="page">
       <div className="page-content">
@@ -20,12 +32,14 @@ export const App: React.FC = () => {
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
+                value={filter}
+                onChange={handleChange}
               />
             </div>
           </div>
         </div>
 
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <div className="sidebar">Sidebar goes here</div>
